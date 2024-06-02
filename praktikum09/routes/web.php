95d43e2755afdcd7ececebc75d795f6079da92b6
daffa157 <muhammadhafidzdaffa70@gmail.com>
2024-06-02 19:11:41 +0700

    tugas12

---
+++ praktikum09/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\PasienController;
+use App\Http\Controllers\PegawaiController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/kabar', function () {
+    return view('kondisi');
+});
+
+Route::get('/pasien', function () {
+    return view('pasien');
+});
+
+Route::get('/admin', [AdminController::class, 'index']);
+Route::get('/admin/pasien', [PasienController::class, 'index'])->name('pasiens.index');
+Route::get('/admin/pasien/create', [PasienController::class, 'create'])->name('pasiens.create');
+Route::post('/admin/pasien/store', [PasienController::class, 'store'])->name('pasiens.store');
+
